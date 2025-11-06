98550dfacee0f0cbe8d379b4227c313afde849d9
jaishreeramharharmahadev <gttechnovation@gmail.com>
2025-11-06 09:43:49 +0530

    B-Jai Shree Ram

--- src/app.js
+++ src/app.js
@@ -1,45 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const cookieParser = require("cookie-parser");
 
+const applicantsRouter = require("./routes/applicants");
+const authRouter = require("./routes/auth");
+const errorHandler = require("./middlewares/errorHandler");
+const rateLimiter = require("./middlewares/rateLimiter");
 
-const applicantsRouter = require('./routes/applicants');
-const authRouter = require('./routes/auth');
-const errorHandler = require('./middlewares/errorHandler');
-const rateLimiter = require('./middlewares/rateLimiter');
-
-const internshipRoutes = require('./routes/internshipRoutes');
+const internshipRoutes = require("./routes/internshipRoutes");
 const certificateRoutes = require("./routes/certificates");
 const feedbackRoutes = require("./routes/feedbackRoutes");
-const { startScheduler } = require('./jobs/scheduler');
-const internshipFeeRoutes = require('./routes/internshipFeeRoutes');
-const whyChooseUsRoutes = require('./routes/whyChooseUsRoutes');
-const testinomialRoutes = require('./routes/testimonialRoutes');
-
+const { startScheduler } = require("./jobs/scheduler");
+const internshipFeeRoutes = require("./routes/internshipFeeRoutes");
+const whyChooseUsRoutes = require("./routes/whyChooseUsRoutes");
+const testinomialRoutes = require("./routes/testimonialRoutes");
 
 const app = express();
 
-
 // Middlewares
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || '*', credentials: true}));
+app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(mongoSanitize());
 app.use(rateLimiter);
 
-
 // Routes
-app.use('/api/applicants', applicantsRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/applicants", applicantsRouter);
+app.use("/api/auth", authRouter);
 
-// Internshi Route 
+// Internshi Route
 app.use("/api/internships", internshipRoutes);
 
-// Certificate Route 
+// Certificate Route
 app.use("/api/certificates", certificateRoutes);
 app.use("/api/uploads", require("./routes/downloads"));
 
@@ -49,22 +45,20 @@ app.use("/api/feedback", feedbackRoutes);
 // Payment
 
 // Fee
-app.use("/api/internship-fees", internshipFeeRoutes)
-app.use("/api/whychooseus", whyChooseUsRoutes)
-app.use("/api/testimonials", testinomialRoutes)
+app.use("/api/internship-fees", internshipFeeRoutes);
+app.use("/api/whychooseus", whyChooseUsRoutes);
+app.use("/api/testimonials", testinomialRoutes);
 
-
-app.get('/', (req, res) => res.send({ ok: true, message: 'Internship backend up' }));
-
+app.get("/", (req, res) =>
+  res.send({ ok: true, message: "Internship backend up" })
+);
 
 if (process.env.SCHEDULER === "true") {
   const { startCertificateScheduler } = require("./jobs/certificateScheduler");
   startCertificateScheduler();
 }
 
-
 // Central error handler
 app.use(errorHandler);
-
 
 module.exports = app;
